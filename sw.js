de856ba843d78e89a01793539ccd0847747b2913
JShicko7 <n5kbgs6hwn@privaterelay.appleid.com>
2026-08-22 11:09:15 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,4 @@
-const CACHE='trifuel-v2-9';
+const CACHE='trifuel-v2-11';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./manifest.webmanifest'])))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{
- if(e.request.mode==='navigate'){
-  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c));return r}).catch(()=>caches.match('./index.html')));
- }else e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c));return r}).catch(()=>caches.match('./index.html')))}else e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
